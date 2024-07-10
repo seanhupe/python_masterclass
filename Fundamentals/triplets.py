ed3237676eb6a2c8fcc3df0accce79950fcaf00a
Sean Hupe <seanhupe@gmail.com>
2024-07-09 18:44:45 -0700

    Update 2

---
+++ Fundamentals/triplets.py
@@ -0,0 +1,9 @@
+# name1 = input("enter the first name: ")
+# name2 = input("enter the second name: ")
+# name3 = input("enter the third name: ")
+#
+# print(f"The names are {name1.strip().capitalize()}, {name2.strip().capitalize()}, {name3.strip().capitalize()}.")
+
+x = input("Give me a number to multiply by 10: ").replace(",", "")
+
+print(int(x) * 10)

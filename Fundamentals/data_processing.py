6aa9030a28aef83e1f9a06a628d6026c5098c4aa
Sean Hupe <seanhupe@gmail.com>
2024-07-09 20:42:55 -0700

    Update 2

--- Fundamentals/data_processing.py
+++ Fundamentals/data_processing.py
@@ -2,8 +2,6 @@
 sentence = input("Enter a short sentence: ").upper()
 print(sentence)
 
-
-
 # 2
 paragraph = input("Enter a paragraph: ")
 # split the paragraph into words
@@ -12,26 +10,22 @@ words = paragraph.split()
 word_count = len(words)
 print(f"The paragraph has {word_count} words.")
 
-
 # 3
 string = input("Enter a string: ")
 # checks if the string is digits (1234)
 is_all_digits = string.isdigit()
 print(f"All characters are digits: {is_all_digits}")
 
-
 # 4
 new_string = input("Please enter another string: ")
 new_string_replace = new_string.replace('a', 'o')
 print(new_string_replace)
-
 
 # 5
 full_name = input("Enter your full name: ")
 name_parts = full_name.split()
 initials = ''.join([part[0].upper() for part in name_parts])
 print(f"Your initials are {initials}")
-
 
 # 6
 last_string = input("Please enter another string: ")

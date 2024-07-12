61173c611ea5245aae07d57d3fffa7f72d768f2d
Sean Hupe <seanhupe@gmail.com>
2024-07-11 19:50:29 -0700

    Update 2

---
+++ Fundamentals/5_summer_func.py
@@ -0,0 +1,15 @@
+def print_coffee_instruction():
+    return "Step 1: Measure for your brew ratio"
+
+
+def five_summer(num1, num2, num3, num4, num5):
+    return num1 + num2 + num3 + num4 + num5
+
+
+print(sum([1, 4, 5, 6, 2]))
+print(len("cat"))
+print(len(["w", "hello", "lol "]))
+
+print(five_summer(3, 4, 2, 9, 12))
+
+print(print_coffee_instruction())

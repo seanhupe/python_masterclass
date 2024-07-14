445af7398fc94358f85ec78d8e880efc9880387f
Sean Hupe <seanhupe@gmail.com>
2024-07-14 14:34:12 -0700

    Update 2

--- Fundamentals/cooking_calculator_func.py
+++ Fundamentals/cooking_calculator_func.py
@@ -2,9 +2,7 @@ turkey_cooking_time = 14
 ham_cooking_time = 5
 
 def cooking_end_time(start_time, duration):
-    end_time = (start_time + duration) % 24
-    return end_time
-
+    return (start_time + duration) % 24
 
 # print(cooking_end_time(13, 8))
 print(f"Take the turkey out of the oven at {cooking_end_time(13, turkey_cooking_time)} o'clock")
--- Fundamentals/misc.py
+++ Fundamentals/misc.py
@@ -1,10 +1,19 @@
-def dollarizer(word):
-    return word.replace('c', 'g')
+# def dollarizer(word):
+#     return word.replace('c', 'g')
+#
+# word = "success"
+# print(dollarizer(word))
 
-word = "success"
-print(dollarizer(word))
+def print_coffee_instructions():
+    return "Step 1: Measure for your brew ratio."
 
 
+print(print_coffee_instructions())
+
+def five_summer(num1, num2, num3, num4, num5):
+    return num1 + num2 + num3 + num4 + num5
+
+print(five_summer(3, 4, 2, 9, 5))
 
 
 

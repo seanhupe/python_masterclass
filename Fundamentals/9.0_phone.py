58d810928043f905dbb25929b722983cbd848cc0
Sean Hupe <seanhupe@yahoo.com>
2024-08-27 21:22:04 -0700

    Update 2

---
+++ Fundamentals/9.0_phone.py
@@ -0,0 +1,11 @@
+# Dictionary
+
+phone_book = {
+    "Alice": 232,
+    "Bob": 123,
+    "Charlie": 2818,
+    }
+
+print(f"Bob's phone # is: {phone_book["Bob"]}")
+print(f"Bob's phone # is: {phone_book["Charlie"]}")
+
--- Fundamentals/9.0_transactions.py
+++ Fundamentals/9.0_transactions.py
@@ -1,4 +1,5 @@
 # Sum of all CC transactions for the month
+from idlelib.pyparse import trans
 
 # transaction1 = 10.20
 # transaction2 = 31.32
@@ -7,8 +8,12 @@
 
 transactions = [10.2, 31.32, 5]
 
-transactions.pop()
+# transactions.pop()
+print(transactions[len(transactions) - 1])
 print(transactions[1])
+print(len(transactions))
+
+transactions.insert(1, 40.8)
+transactions.pop(2)
+print(transactions)
 print(sum(transactions))
-
-

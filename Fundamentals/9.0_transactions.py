9a60d903046d39b72182b17a68833f31e2099cdd
Sean Hupe <seanhupe@yahoo.com>
2024-08-27 20:41:21 -0700

    Update 2

---
+++ Fundamentals/9.0_transactions.py
@@ -0,0 +1,14 @@
+# Sum of all CC transactions for the month
+
+# transaction1 = 10.20
+# transaction2 = 31.32
+# transaction3 = 5
+# print(transaction1 + transaction2 + transaction3)
+
+transactions = [10.2, 31.32, 5]
+
+transactions.pop()
+print(transactions[1])
+print(sum(transactions))
+
+

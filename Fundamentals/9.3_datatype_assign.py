03a89560629057e1403f59f9d23e2548c7066662
Sean Hupe <seanhupe@yahoo.com>
2024-08-29 21:58:08 -0700

    Update 2

---
+++ Fundamentals/9.3_datatype_assign.py
@@ -0,0 +1,35 @@
+# 1. List Manipulation
+
+fruits = ["apple", "banana", "cherry", "date"]
+fruits.append("elderberry")
+fruits.remove("banana")
+print(fruits)
+fruits.sort()
+print(fruits)
+fruits.reverse()
+print(fruits)
+
+#-----------------------------------------------------------------------------
+# 2. Dictionary Basics
+student = {
+    "name": "John Doe",
+    "age": "25",
+    "major": "Computer Science",
+}
+
+print(student)
+
+student["major"] = "Electrical Engineering"
+print(student)
+
+student["Year"] = "Senior"
+print(student)
+
+print(student.keys())
+print(student.values())
+#-----------------------------------------------------------------------------
+
+
+
+
+#-----------------------------------------------------------------------------

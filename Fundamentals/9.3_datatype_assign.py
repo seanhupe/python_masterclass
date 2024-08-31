d5588884005100e6a1aa62e5eb37d4d4997d6b0c
Sean Hupe <seanhupe@yahoo.com>
2024-08-30 19:41:39 -0700

    Update 2

--- Fundamentals/9.3_datatype_assign.py
+++ Fundamentals/9.3_datatype_assign.py
@@ -43,15 +43,36 @@ print(books)
 
 # Add a book to the list
 books.append({"title": "Moby Dick", "author": "Herman Melville", "year": 1851})
-# books.append({"title": "Pride and Prejudice", "author": "Jane Austen", "year": 1813})
+books.append({"title": "Pride and Prejudice", "author": "Jane Austen", "year": 1813})
 # books.append({"title": "IT", "author": "Steven King", "year": 1978})
 print(books)
 
 # Print the title of the second book in the list
-
+print(books[1]["title"])
+#
 # Iterate over the list and print out each Book Title and Author
 for book in books:
     print(f"Title: {book['title']}, author: {book['author']}")
 
 #-----------------------------------------------------------------------------
 # 4. Dictionary containing a List
+courses = {
+    "math": ["Billy", "Timmy", "Shithead"],
+    "history": ["Frank", "Skip", "Jim"],
+    "chemistry": ["David", "John", "Bob"],
+}
+
+# Add 5 students to Math
+courses["math"].extend(["Larry", "Charles", "Peter", "JR", "Kyle"])
+print(courses["math"])
+
+# Remove the third student from history (Jim)
+courses["history"].pop(2)
+print(courses["history"])
+
+# Print out the students in chemistry
+print(courses["chemistry"])
+
+# Add a new course "physics" with 3 students
+courses["physics"] = ["Paul", "Quincy", "Rachel"]
+print(courses)
